f9af1b0cf67ad8a069c85e65e305bab8f80e9d86
Roy <roy@ashbrook.io>
2022-02-23 12:46:49 -0500

    add ssh support

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PSAgentDeploy
-deploy script for PSAgentTemplate
+deploy scripts for PSAgentTemplate
+
+# ssh support
+ssh was added to enable deployment from macs. assumes ssh is already configured with keys and commands can be executed with `ssh user@target "command"` format. must also add a 'user' to settings.json in the jobs section.
+
+this process initially was for deployment from a windows machine to another windows machine, but after switching to a mac there were some changes that had to be made. there seemed to be some hoops to jump through to make remote powershell sessions work on a mac, so since the required functionality for this script was simply copying files and running a command to schedule the task, ssh support was added.
+
+to enable this, need to see the following pages:
+https://techcommunity.microsoft.com/t5/itops-talk-blog/installing-and-configuring-openssh-on-windows-server-2019/ba-p/309540
+
+there is a video there to follow as well, but there are the following issues:
+1. On the repair step, make sure you say NO when asked to give the service account read permissions
+2. On the repair step, i was setting up as another id so it asked to remove inheritance and my logged in id as acccess. i said yes. you can watch the video on the link above to see what the icacls output should look like
+3. when copying the authorized keys, since i was coming from a mac, i had to use / instead of \ in the scp copy path. like below:
+scp ./id_rsa myid@host:C:/Users/myid/.ssh/authorized_keys
+
+some paths and things had to be slightly modified in the deploy.ps1 since we are just running schtasks via ssh instead of using iex, but these were pretty minor.
---
+++ deployssh.ps1
@@ -0,0 +1,125 @@
+<#
+.SYNOPSIS
+Publishes a scheduled job to an agent server
+
+.DESCRIPTION
+Publish a scheduled job to an agent server. Looks in the current directory for a settings.json for configuration information. Will also send notification to a teams channel that a deployment took place.
+
+.EXAMPLE
+Publish-Job
+
+.NOTES
+If settings.json is not found, will not work
+#>
+function Publish-Job{
+    
+  #make sure settings.json is there
+  $checksettings = Test-Path .\settings.json
+  "Test-Path .\settings.json = $checksettings"
+  if ($checksettings){
+
+      #get config
+      $cfg = Get-Content settings.json -Raw | ConvertFrom-Json
+      $job = $cfg.job
+      $p = $job.name
+      $s = $job.server
+      $d = "\\$s\c`$\jobs\$p"
+      $f = $job.files -split ","
+      $t = $job.teams
+      $u = $job.user
+      $ssh = "$u@$s"
+
+      $sb = $null
+      
+      if($job.schedule -eq "every-15-minutes"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc minute /mo 15 /sd 01/01/2001 /st 00:00"}
+      if($job.schedule -eq "hourly-on-the-hour"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc hourly /mo 1 /sd 01/01/2001 /st 00:00"}
+      if($job.schedule -eq "every-6-hours"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc hourly /mo 6 /sd 01/01/2001 /st 00:00"}
+      if($job.schedule -eq "daily-every-3-hours-from-545am-to-845pm"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc daily /sd 01/01/2001 /st 05:45 /du 15:00 /ri (3*60)"}
+      if($job.schedule -eq "daily-at-6am-and-2pm"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc daily /sd 01/01/2001 /st 06:00 /du 10:00 /ri (8*60)"}
+      if($job.schedule -eq "daily-12am"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc daily /sd 01/01/2001 /st 00:00"}
+      if($job.schedule -eq "daily-5am"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc daily /sd 01/01/2001 /st 05:00"}
+      if($job.schedule -eq "daily-230pm"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc daily /sd 01/01/2001 /st 14:30"}
+      if($job.schedule -eq "daily-645pm"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc daily /sd 01/01/2001 /st 18:45"}
+      if($job.schedule -eq "daily-7pm"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc daily /sd 01/01/2001 /st 19:00"}
+      if($job.schedule -eq "weekly-tue-10am"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc weekly /d tue /sd 01/01/2001 /st 10:00"}
+      if($job.schedule -eq "every-january-first"){ $sb = "schtasks /f /create /tn '$p' /tr 'powershell c:\jobs\$p\job.ps1' /ru system /sc monthly /mo 12 /sd 01/01/2001 /st 00:00"}
+
+      # support for pwsh, note this won't work with strictmode
+      if($job.pwsh){
+          $sb = $sb -replace 'powershell','C:\Progra~1\PowerShell\7\pwsh.exe'
+      }
+
+      #make sure we have a valid schedule
+      if($sb){
+
+          #create dir
+          ssh $ssh mkdir $d
+
+          #copy the files
+          scp $f "$($ssh):$d"
+
+          #schedule the job
+          ssh $ssh $sb
+
+          #setup paste for running job now if desired
+          $runtask = "ssh $ssh schtasks /run /tn '$p'"
+          Set-Clipboard $runtask; "To run job now CTRL+V or manually call: $runtask"
+
+          #skip notification if configured
+          if ($job.skipnotification) {return}
+          #get hash link to current commit
+          $h = (git config --get remote.origin.url).replace(".git","") + "/commit/" +  (git log -n1 --format=format:"%H")
+
+          #send publish notification to teams
+          Send-Notification $p $d $s $f $sb $h $t
+
+      }else{
+          throw "no matching schedule for $($job.schedule)"
+      }
+  }else{
+      throw "settings.json missing."
+  }
+  
+
+}
+
+function Send-Notification($p,$d,$s,$f,$sb,$h,$t){
+
+  # webhook uri for teams notification of a deployment
+  $uri = $t
+
+  #json for teams notification
+  $msg = @{
+      "@type"      = "MessageCard"
+      "@context"   = "http://schema.org/extensions"
+      "summary"    = "Publish-Job: Notification"
+      "themeColor" = 'D778D7'
+      "title"      = "$p was published to $d."
+      "sections"   = @(
+          @{
+              "facts" = @(
+                  @{"name"="Job Name:";"value"=$p},
+                  @{"name"="Server:";"value"=$s},
+                  @{"name"="Files:";"value"= $f -join ","},
+                  @{"name"="ScriptBlock:";"value"=$sb},
+                  @{"name"="Commit:";"value"=$h}
+              )
+          "text"  = "Job Details:"
+          }
+      )
+  }
+  $json = convertto-json $msg -depth 4
+  
+  #args for teams notification call
+  $irmargs = @{
+      "URI"         = $uri
+      "Method"      = 'POST'
+      "Body"        = $json
+      "ContentType" = 'application/json'
+  }
+
+  Invoke-RestMethod @irmargs
+
+}
+
+Publish-Job
